c2bde65dfb31465870c850acc0e815bb8a5ba6b7
Vaibhav Parmar <vaibhavparmar2050@gmail.com>
2025-08-16 18:19:00 +0530

    Create Day_2.java
    Day 2 task completed.

---
+++ Day_2.java
@@ -0,0 +1,18 @@
+// Find the Missing Number.
+public class Day_2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5};
+
+        int n = arr.length + 1; 
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        int missingNumber = expectedSum - actualSum;
+
+        System.out.println("Missing number: " + missingNumber);
+    }
+}
