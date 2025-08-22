bb071c4db800cdca5f0bea28254f8b046b24b721
Vaibhav Parmar <vaibhavparmar2050@gmail.com>
2025-08-22 23:08:29 +0530

    Create Day_8.java
    Day 8 task completed.

---
+++ Day_8.java
@@ -0,0 +1,24 @@
+//  Reverse a String Word by Word
+
+class DPP_8 {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        
+        StringBuilder result = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i > 0) result.append(" ");
+        }
+        
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        DPP_8 solver = new DPP_8();
+        String input = "the sky is blue";
+        System.out.println(solver.reverseWords(input)); 
+
+        String input2 = "  hello   world!  ";
+        System.out.println(solver.reverseWords(input2));
+    }
+}
